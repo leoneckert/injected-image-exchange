566180dbca7d10067bb4aca4b708a1fc670b4f61
Joakim Gregersen Quach <joakimquach@gmail.com>
2015-11-11 23:02:11 -0500

    Update redirect_admin.py

--- redirect_admin.py
+++ redirect_admin.py
@@ -51,11 +51,6 @@
 from libmproxy.protocol.http import HTTPResponse
 from netlib.odict import ODictCaseless
 
-"""
-This example shows two ways to redirect flows to other destinations.
-"""
-
-
 def request(context, flow):
     # pretty_host(hostheader=True) takes the Host: header of the request into account,
     # which is useful in transparent mode where we usually only have the IP
